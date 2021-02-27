e053ae2cd2759683e579ebf353229d963d3cccce
Danil <dinkodanil@gmail.com>
2021-02-27 04:43:59 +0300

    Setting deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,8 +1,10 @@
-class TypeController {
+class DeviceController {
 
     async create(req, res) {}
 
     async getAll(req, res) {}
+
+    async getOne(req, res) {}
 }
 
-module.exports = new TypeController()
+module.exports = new DeviceController()
