dc76342da4bac7dea15b38c958f2ce924b66e0b3
Danil <dinkodanil@gmail.com>
2021-02-27 05:34:19 +0300

    Add in deviceController create function

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,8 +1,27 @@
+const uuid = require("uuid")
+const path = require("path")
+
+const {Device} = require("../models/models")
+const ApiError = require("../error/ApiError")
+
+
 class DeviceController {
 
-    async create(req, res) {
-        const {name, price, brandId, typeId, info} = req.body
-        const {img} = req.files
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const device = await Device.create({name, price, brandId, typeId, img: fileName })
+
+            return res.json(device)
+        } catch (e) {
+            console.log('Error:', e)
+            next(ApiError.badRequest(e.message))
+        }
     }
 
     async getAll(req, res) {}
