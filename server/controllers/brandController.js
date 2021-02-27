66ed0496bf9a417e4c46f7a92655829148b249a4
Danil <dinkodanil@gmail.com>
2021-02-27 05:16:01 +0300

    Add brandController and refactor typeController

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,8 +1,23 @@
+const {Brand} = require('../models/models')
+const ApiError = require('../error/ApiError')
+
+
 class BrandController {
 
-    async create(req, res) {}
+    async create(req, res, next) {
+        const {name} = req.body
 
-    async getAll(req, res) {}
+        const isUnique = await Brand.findOne({name})
+        if(isUnique) {
+            return next(ApiError.arleadyHas('Этот брэнд уже существует'))
+        }
+
+        return res.json(await Brand.create({name}))
+    }
+
+    async getAll(req, res) {
+        return res.json(await Brand.findAll())
+    }
 }
 
 module.exports = new BrandController()
