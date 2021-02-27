4b1898eb57d58d9ed050d81abf4e9e3f8d5fda66
Danil <dinkodanil@gmail.com>
2021-02-27 05:04:18 +0300

    Setting routes with controllers classes

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,10 @@
 const Router = require("express")
 
+const brandController = require('../controllers/brandController')
+
 const router = new Router()
 
-router.post('/', )
-router.get('/', )
+router.post('/', brandController.create)
+router.get('/', brandController.getAll)
 
 module.exports = router
