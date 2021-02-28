999e52dbd2d184fe1e6d88043dd2b0ad42a61f20
Danil <dinkodanil@gmail.com>
2021-02-28 20:37:29 +0300

    Add checkRoleMiddleware function

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken")
+const config = require("config")
+
+
+module.exports = function(role) {
+    return function(req, res, next) {
+        if(req.method === "OPTIONS") {
+            next()
+        }
+
+        try {
+            const token = req.headers.authorization.split(' ')[1]
+            if(!token) {
+                return res.status(403).json({message: "Пользователь не авторизован"})
+            }
+
+            const decodedData = jwt.verify(token, process.env.SECRET_KEY)
+            if(decodedData.role != role) {
+                return res.status(403).json({message: "Ваша роль не соответствует требуемой"})
+            }
+            req.user = decodedData
+            next()
+        } catch (e) {
+            console.log('Error:', e)
+            return res.status(403).json({message: "Пользователь не авторизован"})
+        }
+}
+
+
+
+}
