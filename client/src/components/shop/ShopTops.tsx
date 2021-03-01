ab15ca936b786d183b44afae6c6d52dde033905e
Danil <dinkodanil@gmail.com>
2021-03-02 00:59:32 +0300

    Create ShopTops component

---
+++ client/src/components/shop/ShopTops.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const ShopTops = () => {
+    return (
+        <div className="devices__last">
+            <div className="devices-brands">
+                <div className="devices-brands__item">
+                    <div className="devices-brands__item__tag">
+                        <img src="" alt=""/>
+                        <span>Brand</span>
+                    </div>
+
+                    <div className="devices-brands__item__name">
+                        SAMSUNG
+                    </div>
+
+                    <div className="devices-brands__item__view">
+                        <span>View</span>
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+            </div>
+            <div className="devices-types">
+                <div className="devices-types__item">
+                    <div className="devices-types__item__tag">
+                        <img src="" alt=""/>
+                        <span>Brand</span>
+                    </div>
+
+                    <div className="devices-types__item__name">
+                        SAMSUNG
+                    </div>
+
+                    <div className="devices-types__item__view">
+                        <span>View</span>
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShopTops;
--- client/src/components/shop/index.tsx
+++ client/src/components/shop/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './shop.sass'
 import SearchInfo from "./SearchInfo";
+import ListCardDevices from "./cards/ListCardDevices";
+import ShopTops from "./ShopTops";
 
 const Shop = () => {
     return (
@@ -9,42 +11,8 @@ const Shop = () => {
             <div className="devices">
                 <h1>Devices</h1>
                 <SearchInfo />
-                <div className="devices__last">
-                    <div className="devices-brands">
-                        <div className="devices-brands__item">
-                            <div className="devices-brands__item__tag">
-                                <img src="" alt=""/>
-                                <span>Brand</span>
-                            </div>
-
-                            <div className="devices-brands__item__name">
-                                SAMSUNG
-                            </div>
-
-                            <div className="devices-brands__item__view">
-                                <span>View</span>
-                                <img src="" alt=""/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="devices-types">
-                        <div className="devices-types__item">
-                            <div className="devices-types__item__tag">
-                                <img src="" alt=""/>
-                                <span>Brand</span>
-                            </div>
-
-                            <div className="devices-types__item__name">
-                                SAMSUNG
-                            </div>
-
-                            <div className="devices-types__item__view">
-                                <span>View</span>
-                                <img src="" alt=""/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <ShopTops />
+                <ListCardDevices />
             </div>
         </div>
     );
