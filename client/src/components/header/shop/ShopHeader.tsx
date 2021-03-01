fc2a352c6f8e5ec4f67665f4af153932f948c3da
Danil <dinkodanil@gmail.com>
2021-03-01 23:26:51 +0300

    Create html structure in ShopHeader

--- client/src/components/header/shop/ShopHeader.tsx
+++ client/src/components/header/shop/ShopHeader.tsx
@@ -6,8 +6,31 @@ import { Link } from 'react-router-dom';
 const ShopHeader = ({dispatch, activeItem}: {dispatch: any, activeItem: string}) => {
     return (
         <header>
-            <Link to={"shop"} className="login-link-active">Sex in</Link>
-            <Link to={"shop"} onClick={() => dispatch(logoutUser())} className="reg-link">Logout</Link>
+            <p>STRIKE SHOP</p>
+            <div className="links">
+                <Link
+                    to={"shop"}
+                    className="login-link-active"
+                >
+                    Devices
+                </Link>
+                <Link
+                    to={"brands"}
+                    onClick={() => dispatch(logoutUser())}
+                    className="reg-link"
+                >
+                    Brands
+                </Link>
+                <Link
+                    to={"types"}
+                    onClick={() => dispatch(logoutUser())}
+                    className="reg-link"
+                >
+                    Types
+                </Link>
+
+                <div className="links__basket">Basket</div>
+            </div>
         </header>
     );
 };
