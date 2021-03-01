0326a5343ee7d5500db1130f99b946e8b152aebd
Danil <dinkodanil@gmail.com>
2021-03-02 00:57:39 +0300

    Create shop html structure

--- client/src/components/shop/index.tsx
+++ client/src/components/shop/index.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import './shop.sass'
+import SearchInfo from "./SearchInfo";
 
 const Shop = () => {
     return (
-        <div>
-            Shop
+        <div className={"shop"}>
+            {/*Slider*/}
+            <div className="devices">
+                <h1>Devices</h1>
+                <SearchInfo />
+                <div className="devices__last">
+                    <div className="devices-brands">
+                        <div className="devices-brands__item">
+                            <div className="devices-brands__item__tag">
+                                <img src="" alt=""/>
+                                <span>Brand</span>
+                            </div>
+
+                            <div className="devices-brands__item__name">
+                                SAMSUNG
+                            </div>
+
+                            <div className="devices-brands__item__view">
+                                <span>View</span>
+                                <img src="" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="devices-types">
+                        <div className="devices-types__item">
+                            <div className="devices-types__item__tag">
+                                <img src="" alt=""/>
+                                <span>Brand</span>
+                            </div>
+
+                            <div className="devices-types__item__name">
+                                SAMSUNG
+                            </div>
+
+                            <div className="devices-types__item__view">
+                                <span>View</span>
+                                <img src="" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
