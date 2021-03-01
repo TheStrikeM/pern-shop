ac321cc2187b0faeb4a6e3c808a81a48c3442557
Danil <dinkodanil@gmail.com>
2021-03-01 19:35:59 +0300

    ADd footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,1 +1,12 @@
-export {}
+import React from 'react'
+import './footer.sass'
+
+const Footer = () => {
+    return (
+        <footer>
+            Pern-shop by TheStrikeM special for Github
+        </footer>
+    );
+};
+
+export default Footer;
