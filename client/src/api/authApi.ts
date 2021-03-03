a4c246b3c10ef84e40ac8a10ada43227043aab6c
Danil <dinkodanil@gmail.com>
2021-03-03 21:18:43 +0300

    Final developing AuthApi, add auth, login, register instance functions

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -4,7 +4,7 @@ import {ApiDefaultResponseType, instance, UserReponseType} from "./index";
 type AuthApiResponseType = ApiDefaultResponseType<UserReponseType>
 
 export default class AuthApi {
-    reg(email: string, password: string) {
+    register(email: string, password: string) {
         return instance.post<AuthApiResponseType>('user/reg', {
             email, password
         }).then(res => res.data)
@@ -13,6 +13,12 @@ export default class AuthApi {
     login(email: string, password: string) {
         return instance.post<AuthApiResponseType>('user/login', {
             email, password
+        }).then(res => res.data)
+    }
+
+    auth(token: string) {
+        return instance.get<AuthApiResponseType>('user/auth', {
+            headers: {Authorization: `Bearer ${token}`}
         }).then(res => res.data)
     }
 }
