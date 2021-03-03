27853289b89298a91a86c728d424ccb33fec0f9a
Danil <dinkodanil@gmail.com>
2021-03-03 21:13:55 +0300

    Change object to class

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -3,12 +3,13 @@ import {ApiDefaultResponseType, instance, UserReponseType} from "./index";
 
 type AuthApiResponseType = ApiDefaultResponseType<UserReponseType>
 
-export const authApi = {
+export default class AuthApi {
     reg(email: string, password: string) {
         return instance.post<AuthApiResponseType>('user/reg', {
             email, password
         }).then(res => res.data)
-    },
+    }
+
     login(email: string, password: string) {
         return instance.post<AuthApiResponseType>('user/login', {
             email, password
