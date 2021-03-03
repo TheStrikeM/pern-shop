b44b10398a2a67f93a4f9086ba6ac4cdb9c16e92
Danil <dinkodanil@gmail.com>
2021-03-03 23:13:09 +0300

    Remove personal action creator types

--- client/src/types/actionTypes.ts
+++ client/src/types/actionTypes.ts
@@ -1,14 +1,2 @@
-import {LOGOUT, SET_USER} from "../utils/constants";
-import {UserResponseType} from "../api";
-
-export type SetUserActionType = {
-    type: typeof SET_USER,
-    payload: UserResponseType
-}
-
-export type LogoutType = {
-    type: typeof LOGOUT
-}
-
 type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
 export type InferAuthActionTypes<T extends {[key: string]: (...args: any[]) => any} > = ReturnType<PropertiesTypes<T>>
